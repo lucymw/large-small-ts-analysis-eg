80f40e905d9a53a99268ce60ff08d920db543ed0
Lucille Watkins <lucymw@ufl.edu>
2016-11-18 12:13:39 -0500

    Research question and general information about what the r-code will attempt to do

---
+++ houseelf-analysis.R
@@ -0,0 +1,3 @@
+#Determine the relationship between elf ear length and GC content of corresponding
+#DNA samples as part of a study to understand why house elves (like Dobby) 
+#possess powerful magic
