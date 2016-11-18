3540f00aadb42241c3bae03e3fee8daba86bf6c2
Lucille Watkins <lucymw@ufl.edu>
2016-11-18 12:32:06 -0500

    Download data from website. Read data into object in R (elf_data)

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,3 +1,8 @@
 #Determine the relationship between elf ear length and GC content of corresponding
 #DNA samples as part of a study to understand why house elves (like Dobby) 
 #possess powerful magic
+
+download.file("http://www.datacarpentry.org/semester-biology/data/houseelf-earlength-dna-data.csv",
+              "data/houseelf-earlength-dna-data.csv")
+
+elf_data <- read.csv("data/houseelf-earlength-dna-data.csv", stringsAsFactors = FALSE)
